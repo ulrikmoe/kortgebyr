222b06298f3a61bd2cb25fe006b38db929edccde
Ulrik Moe <ulrik.moe@gmail.com>
2013-06-17 05:59:14 +0200

    ok

--- kortgebyr.js
+++ kortgebyr.js
@@ -2,11 +2,12 @@ function init() {
 
 	transactions = document.getElementById("transactions").value;
 	value = document.getElementById("value").value;
-			
+
 	PSP = [
-	
+
 		{
 			name: "paypal",
+			logo: "paypal.png",
 			komplet: true,
 			link: "paypal.com",
 			setupFee: 0,
@@ -18,6 +19,7 @@ function init() {
 		},
 		{
 			name: "ewire",
+			logo: "ewire.png",
 			komplet: true,
 			link: "paypal.com",
 			setupFee: 1195,
@@ -29,6 +31,7 @@ function init() {
 		},
 		{
 			name: "ewire",
+			logo: "ewire.png",
 			komplet: true,
 			link: "paypal.com",
 			setupFee: 395,
@@ -40,6 +43,7 @@ function init() {
 		},
 		{
 			name: "skrill",
+			logo: "skrill.png",
 			komplet: true,
 			link: "paypal.com",
 			setupFee: 0,
@@ -51,74 +55,166 @@ function init() {
 		},
 		{
 			name: "quickpay",
+			logo: "quickpay.png",
 			komplet: false,
 			link: "paypal.com",
 			setupFee: 0,
 			monthlyFee: 150,
 			fixedTransactionFee: 0,
-			variableTransactionFee: 3.4,
+			variableTransactionFee: 0,
 			totalTransactionCosts: 0,
 			totalCosts: 0
 		},
 		{
-			name: "epay",
+			name: "ePay Light",
+			logo: "epay.png",
 			komplet: false,
-			link: "paypal.com",
+			link: "epay.dk",
+			setupFee: 399,
+			monthlyFee: 99,
+			fixedTransactionFee: 0.25,
+			variableTransactionFee: 0,
+			totalTransactionCosts: 0,
+			totalCosts: 0
+		},
+		{
+			name: "ePay Pro",
+			logo: "epay.png",
+			komplet: false,
+			link: "epay.dk",
+			setupFee: 599,
+			monthlyFee: 199,
+			fixedTransactionFee: 0.25,
+			variableTransactionFee: 0,
+			totalTransactionCosts: 0,
+			totalCosts: 0
+		},
+		{
+			name: "ePay Business",
+			logo: "epay.png",
+			komplet: false,
+			link: "epay.dk",
 			setupFee: 999,
 			monthlyFee: 299,
 			fixedTransactionFee: 0.25,
-			variableTransactionFee: 3.4,
+			variableTransactionFee: 0,
+			totalTransactionCosts: 0,
+			totalCosts: 0
+		},
+		{
+			name: "DIBS Entrepreneur",
+			logo: "dibs.png",
+			komplet: false,
+			link: "dibs.dk",
+			setupFee: 0,
+			monthlyFee: 149,
+			fixedTransactionFee: 3,
+			variableTransactionFee: 0,
 			totalTransactionCosts: 0,
 			totalCosts: 0
 		},
 	  	{
-			name: "dibs",
+			name: "DIBS Business",
+			logo: "dibs.png",
 			komplet: false,
-			link: "paypal.com",
+			link: "dibs.dk",
+			setupFee: 0,
+			monthlyFee: 249,
+			fixedTransactionFee: 3,
+			variableTransactionFee: 0,
+			totalTransactionCosts: 0,
+			totalCosts: 0
+		},
+		{
+			name: "DIBS Professional",
+			logo: "dibs.png",
+			komplet: false,
+			link: "dibs.dk",
 			setupFee: 0,
 			monthlyFee: 249,
 			fixedTransactionFee: 1.5,
-			variableTransactionFee: 3.4,
+			variableTransactionFee: 0,
 			totalTransactionCosts: 0,
 			totalCosts: 0
 		},
 		{
-			name: "dibs",
+			name: "Netaxept start",
+			logo: "netaxept.png",
 			komplet: false,
-			link: "paypal.com",
-			setupFee: 0,
-			monthlyFee: 249,
-			fixedTransactionFee: 1,
-			variableTransactionFee: 3.4,
+			link: "terminalshop.dk",
+			setupFee: 1000,
+			monthlyFee: 180,
+			fixedTransactionFee: 1.5,
+			variableTransactionFee: 0,
 			totalTransactionCosts: 0,
 			totalCosts: 0
 		},
 		{
-			name: "netaxept",
+			name: "Netaxept plus",
+			logo: "netaxept.png",
 			komplet: false,
-			link: "paypal.com",
-			setupFee: 7500,
-			monthlyFee: 700,
-			fixedTransactionFee: 0.7,
-			variableTransactionFee: 3.4,
-			totalTransactionCosts: 0,
-			totalCosts: 0
-		},
-		{
-			name: "netaxept",
-			komplet: false,
-			link: "paypal.com",
+			link: "terminalshop.dk",
 			setupFee: 3000,
 			monthlyFee: 500,
 			fixedTransactionFee: 1,
-			variableTransactionFee: 3.4,
+			variableTransactionFee: 0,
+			totalTransactionCosts: 0,
+			totalCosts: 0
+		},
+		{
+			name: "Netaxept advanced",
+			logo: "netaxept.png",
+			komplet: false,
+			link: "terminalshop.dk",
+			setupFee: 7500,
+			monthlyFee: 700,
+			fixedTransactionFee: 0.7,
+			variableTransactionFee: 0,
+			totalTransactionCosts: 0,
+			totalCosts: 0
+		},
+		{
+			name: "Dandomain",
+			logo: "dandomain.png",
+			komplet: false,
+			link: "dandomain.dk",
+			setupFee: 199,
+			monthlyFee: 149,
+			fixedTransactionFee: 0,
+			variableTransactionFee: 0,
+			totalTransactionCosts: 0,
+			totalCosts: 0
+		},
+		{
+			name: "FreePay",
+			logo: "freepay.png",
+			komplet: false,
+			link: "freepay.dk",
+			setupFee: 0,
+			monthlyFee: 349,
+			fixedTransactionFee: 0,
+			variableTransactionFee: 0,
+			totalTransactionCosts: 0,
+			totalCosts: 0
+		},
+		{
+			name: "Wannafind",
+			logo: "wannafind.png",
+			komplet: false,
+			link: "wannafind.dk",
+			setupFee: 0,
+			monthlyFee: 149,
+			fixedTransactionFee: 0,
+			variableTransactionFee: 0,
 			totalTransactionCosts: 0,
 			totalCosts: 0
 		}
-	  
+
+
+
 	];
-	
-	
+
+
 	nets_setupFee = 1000;
 	nets_monthlyFee = 83.33;
 	nets_fixedTransactionFee = 0.7;
@@ -131,25 +227,25 @@ function init() {
 	seb_fixedTransactionFees = 0;
 	seb_variableTransactionFees = 1.6;
 	seb_total = 0;
-	
-	
+
+
 	oms = transactions * value;
 
 	calc();
-	
+
 }
 
 
 function calc() {
 
 	/*
-	
+
 	==========================================
 	P	A	Y	P	A	L
 	==========================================
 
 	*/
-	
+
 	if (oms < 20000 ) { PSP[0].variableTransactionFee = 3.4; }
 	else if (oms <= 80000) { PSP[0].variableTransactionFee = 2.9; }
 	else if (oms <= 400000) { PSP[0].variableTransactionFee = 2.7; }
@@ -159,7 +255,7 @@ function calc() {
 
 
 	/*
-	
+
 	==========================================
 	S	K	R	I	L	L
 	==========================================
@@ -171,7 +267,7 @@ function calc() {
 
 
 	/*
-	
+
 	==========================================
 	N	E	T	S
 	==========================================
@@ -181,30 +277,30 @@ function calc() {
 	if ( value <= 50 ) { nets_fixedTransactionFee = 0.7; }
 	else if ( value <= 100 ) { nets_fixedTransactionFee = 1.1; }
 	else if ( value <= 500 ) { nets_fixedTransactionFee = 1.45; }
-	else { nets_fixedTransactionFee = 1.45; nets_percentages = 0.001 }	
-	
+	else { nets_fixedTransactionFee = 1.45; nets_percentages = 0.001 }
+
 
 	nets_total = 83.33 + 0.8 * ( transactions * nets_fixedTransactionFee + oms * nets_variableTransactionFee );
-	
+
 	seb_total = 0.2 * oms * 0.016;
-	
-	
-	
-	
-	
+
+
+
+
+
 	/*
 		Her udregner jeg transaktionsgebyrer + total
 	*/
-	
+
 	for (var i=0; i<PSP.length; i++)
 	{
-	
-	
+
+
 		if ( PSP[i].komplet == true )
 		{
 
 			// PSP[0].variableTransactionFee
-			
+
 			PSP[i].totalTransactionCosts = ( PSP[i].fixedTransactionFee * transactions ) + ( PSP[i].variableTransactionFee/100 * oms ) ;
 
 			PSP[i].totalCosts =	PSP[i].monthlyFee + PSP[i].totalTransactionCosts;
@@ -215,7 +311,7 @@ function calc() {
 
 			if ( i == 5 )
 			{
-				
+
 				if ( transactions > 500 )
 				{
 
@@ -223,29 +319,29 @@ function calc() {
 
 				}
 				else {
-					
+
 					PSP[i].totalTransactionCosts =  nets_total + seb_total;
-					
+
 				}
 
-				
+
 			}
 			else {
 
 				PSP[i].totalTransactionCosts = ( PSP[i].fixedTransactionFee * transactions ) + nets_total + seb_total;
 
 			}
-			
-			
+
+
 			PSP[i].totalCosts =	PSP[i].monthlyFee + PSP[i].totalTransactionCosts;
 
-			
+
 
 		}
 
 	}
-	
-		
+
+
 	build();
 
 }
@@ -257,7 +353,7 @@ function build() {
 
 
 	PSP.sort(function (b, a) {
-		
+
 		return a.totalCosts - b.totalCosts;
 
 	});
@@ -280,11 +376,11 @@ function build() {
 		var cell5=row.insertCell(4);
 
 
-		cell1.innerHTML = "<img src='logo/"+PSP[i].name+".png' />";
-		cell2.innerHTML = PSP[i].setupFee;
-		cell3.innerHTML = PSP[i].monthlyFee;
-		cell4.innerHTML = PSP[i].totalTransactionCosts;
-		cell5.innerHTML = PSP[i].totalCosts;
+		cell1.innerHTML = "<img style='margin:3px 0 3px;' height='40' src='logo/"+PSP[i].logo+"' />"+PSP[i].name;
+		cell2.innerHTML = Math.round(PSP[i].setupFee) + " kr";
+		cell3.innerHTML = Math.round(PSP[i].monthlyFee) + " kr";
+		cell4.innerHTML = Math.round(PSP[i].totalTransactionCosts) + " kr";
+		cell5.innerHTML = Math.round(PSP[i].totalCosts) + " kr";
 
 
 
@@ -300,11 +396,11 @@ function gateways() {
 
 	for (var i=0; i< gateway_monthly.length; i++)
 	{
-					
-		
+
+
 		var total = gateway_monthly[i]+nets+seb;
 
-		
+
 		if ( ( i == 0 ) && ( transactions > 500 ) ) {
 		// Quickpay
 
@@ -318,36 +414,36 @@ function gateways() {
 		*/
 
 			var quickpayFees = 0;
-		
+
 
 			if ( transactions < 601 )
 			{
-				
+
 				quickpayFees += ( transactions - 500 ) * 0.5 ;
-									
+
 			}
 			else if ( transactions < 1001 )
 			{
-				
+
 				quickpayFees += 99 * 0.5 ;
 				quickpayFees += ( transactions - 600 ) * 0.4 ;
 			}
-			
+
 			else if ( transactions < 3001 )
 			{
-				
+
 				quickpayFees += 99 * 0.5 ;
 				quickpayFees += 400 * 0.4 ;
 			}
-			
+
 
 			total = total + quickpayFees;
-							
+
 
 		}
 		else if ( ( i == 1 ) && ( transactions > 500 ) ) {
 		// epay
-			
+
 			total = total + ( (transactions - 500) *  0.25);
 
 		}
@@ -357,7 +453,7 @@ function gateways() {
 
 		}
 		else if ( i == 3 ) {
-			
+
 			total = total + (transactions *  1);
 
 		}
@@ -366,7 +462,7 @@ function gateways() {
 			total = total + (transactions *  0.7);
 
 		}
-			
+
 		document.getElementById("gateway_total_"+i).innerHTML = total;
 
 
