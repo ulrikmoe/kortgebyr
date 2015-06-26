ccc678fafc7d9b5573912590e987682911e123d7
Ulrik Moe <ulrik.moe@gmail.com>
2015-06-26 10:54:34 +0200

    Removed Quickpay's old platform

--- src/js/kortgebyr.js
+++ src/js/kortgebyr.js
@@ -739,39 +739,6 @@ var psps = { // alfabetisk rækkefølge
     }
   },
 
-  "quickpay": {
-    name: "quickpay (old platform)",
-    logo: "quickpay.png",
-    link: "https://quickpay.net",
-    is_acquirer: false,
-    acquirers: ["nets", "euroline", "teller", "swedbank", "Elavon", "handelsbanken"],
-    cards: ["dankort", "visa", "mastercard", "maestro"],
-    costfn: function (o) {
-
-      if (o.multiacquirer) { return null; }
-
-      var limits = [0, 500, 600, 1000, 3000, 10000, 30000];
-      var fees = [0, 0.5, 0.4, 0.3, 0.25, 0.15, 0.1];
-      var price = 0;
-      var accum = 0;
-      for (var i = 0; i < limits.length; i++) {
-        var delta;
-        if (i < limits.length - 1) {
-          delta = Math.min(limits[i + 1] - limits[i], o.n - accum);
-        } else {
-          delta = o.n - accum;
-        }
-        price += delta * fees[i];
-        accum += delta;
-      }
-      return {
-        setup: new Currency(0, 'DKK'),
-        monthly: new Currency(150, 'DKK'),
-        trans: new Currency(price, 'DKK')
-      };
-    }
-  },
-
   "quickpaystandard": {
     name: "QuickPay Standard",
     logo: "quickpay.png",
