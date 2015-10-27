5514b1e527d7c9af86befa502884e0c524523474
Ulrik Moe <ulrik.moe@gmail.com>
2015-10-27 22:46:11 +0100

    Clean up

--- publish.sh
+++ publish.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-timestamp=$(date +"%Y%m%d%H%M")
+hostname=scanpay.dk
+folder=kortgebyr.dk
+filename=$(date +"%Y%m%d%H%M")
 
+#  Build website
 gulp clean
 gulp build --lang da
 
-# Gzip files{html,css,js,svg}
+#  Gzip: css, js, html
 echo -n "Gzipping"
-for file in $(find www -name '*.css' -or -name '*.js' -or -name '*.svg' -or -name '*.html'); do
+for f in $(find www -name '*.css' -or -name '*.js' -or -name '*.svg' -or -name '*.html'); do
    if [ -z "$(which zopfli)" ]; then
-      gzip -9 < $file > $file.gz
+      gzip -9 < $f > $f.gz
    else
       echo -n "."
-      zopfli --i1000 $file;
+      zopfli --i1000 $f;
    fi
-done
-echo ""
+done; echo ""
 
-## Create tar.xz
-tar -Jcf $timestamp.tar.xz www
+#  Make tarball of 'www' folder
+tar -Jcf $filename.tar.xz www
 
-## Upload and untar
-rsync --remove-source-files --progress -e ssh $timestamp.tar.xz scanpay.dk:/srv/kortgebyr.dk/
-ssh scanpay.dk "cd /srv/kortgebyr.dk; rm -rf www; tar xfJ $timestamp.tar.xz"
+#  Upload to server and untar at /srv/$folder/
+rsync --remove-source-files --progress -e ssh $filename.tar.xz $hostname:/srv/$folder
+ssh $hostname "cd /srv/$folder; rm -rf old; mv www old; tar xfJ $filename.tar.xz"
