ef8c78cae7658146b134fd9e9a93780adec7a559
Ulrik Moe <ulrik.moe@gmail.com>
2015-11-23 22:45:27 +0100

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 /**
-*   gulpfile.js
-*   @author Ulrik Moe. Copyright (c) 2015.
+*   @author Ulrik Moe.
 *   @license GPLv3
 *
 *   Indentation: 3 spaces
@@ -39,12 +38,11 @@ var config = {
 };
 
 var paths = {
-   dest: 'www',
    html: 'src/*.html',
-   scripts: 'src/js/*.js',
+   js: 'src/js/*.js',
    less: 'src/css/*.less',
    rebuild: ['www/all.js', 'www/global.css'],
-   assets: ['src/fonts/**','src/img/**']
+   assets: ['src/**/*.{pdf,png,svg,jpg,jpeg,woff,woff2,ico}']
 };
 
 // Last changed
@@ -55,40 +53,27 @@ var month = datetime.getMonth();
 var year = datetime.getFullYear();
 var lastUpdate = day + ". " + monthNames[month] + " " + year;
 
-// Call with 'gulp clean'
-function clean(done) {
-   del([paths.dest], done);
-}
-
-function webserver() {
-   connect.server({
-      root: paths.dest,
-      livereload: config.dev,
-      port: 9000
-   });
-}
-
-function assets() {
-   return gulp.src(paths.assets, { base: './src/' })
-      .pipe(gulp.dest(paths.dest));
-}
+// Gulp tasks
+function clean(done) { return del(['www/**', '!www'], done); }
+function assets() { return gulp.src(paths.assets).pipe( gulp.dest('www') ); }
+function webserver() { connect.server({ root:'www', livereload:config.dev, port:9000 }); }
 
 function scripts() {
-   return gulp.src(paths.scripts, { base: paths.src })
+   return gulp.src(paths.js)
       .pipe(concat('all.js'))
       .pipe(!config.dev ? uglify(config.uglify) : gutil.noop())
-      .pipe(gulp.dest(paths.dest));
+      .pipe(gulp.dest('www'));
 }
 
 function less2css() {
-   return gulp.src(paths.less, { base: paths.src })
+   return gulp.src(paths.less)
       .pipe(less())
       .pipe(!config.dev ? minifyCSS() : gutil.noop())
-      .pipe(gulp.dest(paths.dest));
+      .pipe(gulp.dest('www'));
 }
 
 function html() {
-   return gulp.src(paths.html, { base: paths.src })
+   return gulp.src(paths.html)
       .pipe(nunjucks({
          searchPaths: ['www/'],
          locals: {
@@ -96,7 +81,7 @@ function html() {
          }
       }))
       .pipe(!config.dev ? htmlmin(config.htmlmin) : gutil.noop())
-      .pipe(gulp.dest(paths.dest))
+      .pipe(gulp.dest('www'))
       .pipe(connect.reload());
 }
 
@@ -104,7 +89,7 @@ function stalker() {
    gulp.watch(paths.less, less2css);
    gulp.watch(paths.assets, assets);
    gulp.watch(paths.html, html);
-   gulp.watch(paths.scripts, scripts);
+   gulp.watch(paths.js, scripts);
    gulp.watch(paths.rebuild, html);
 }
 
