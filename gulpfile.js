9062eb7e94e0916b9fcd56c8b89cdc939dbce8a6
Ulrik Moe <ulrik.moe@gmail.com>
2015-12-06 12:16:17 +0100

    More gulp improvements and fixes

--- gulpfile.js
+++ gulpfile.js
@@ -11,22 +11,30 @@
 **/
 
 var gulp = require('gulp');
+var del = require('del');
 var gutil = require('gulp-util');
 var connect = require('gulp-connect');
 var uglify = require('gulp-uglify');
 var nunjucks = require('gulp-nunjucks-html');
 var htmlmin = require('gulp-htmlmin');
-var del = require('del');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-minify-css');
+
+//  Last changed
+var monthNames = ["januar", "februar", "marts", "april", "maj", "juni", "juli", "august", "september", "oktober", "november", "december"];
+var date = new Date();
+var updated = date.getDate() + ". " + monthNames[date.getMonth()] + " " + date.getFullYear();
 
 var config = {
    lang: (gutil.env.lang ? gutil.env.lang : 'da'),
    dev: !!gutil.env.dev,
    htmlmin: {
       removeComments: true,
-      collapseWhitespace: true,
-      minifyJS: true
+      collapseWhitespace: true
+   },
+   nunjucks: {
+      searchPaths: ['www/'],
+      locals: { lastUpdate: updated }
    }
 };
 
@@ -37,14 +45,8 @@ var paths = {
    assets: ['src/fonts/**','src/img/**']
 };
 
-//  Last changed
-var monthNames = ["januar", "februar", "marts", "april", "maj", "juni", "juli", "august", "september", "oktober", "november", "december"];
-var date = new Date();
-var lastUpdate = date.getDate() + ". " + monthNames[date.getMonth()] + " " + date.getFullYear();
-
 // Gulp tasks
-function clean() { return del(['www/**', '!www']); }
-function server() { connect.server({ root: 'www', livereload: true }); }
+function server() { connect.server({ root: 'www', livereload: config.dev }); }
 
 function assets() {
    return gulp.src(paths.assets, {base:'src', since: gulp.lastRun(assets)})
@@ -67,10 +69,7 @@ function less2css() {
 
 function html() {
    return gulp.src(paths.html)
-      .pipe(nunjucks({
-         searchPaths: ['www/'],
-         locals: { lastUpdate: lastUpdate }
-      }))
+      .pipe(nunjucks(config.nunjucks))
       .pipe(!config.dev ? htmlmin(config.htmlmin) : gutil.noop())
       .pipe(gulp.dest('www'))
       .pipe(connect.reload());
@@ -83,9 +82,9 @@ function stalker() {
    gulp.watch(paths.js, gulp.series(scripts, html));
 }
 
-gulp.task(clean);
+gulp.task('clean', function() { return del(['www/**', '!www']); });
 gulp.task('build', gulp.series(assets, scripts, less2css, html));
 gulp.task('default', gulp.series(
-   clean, assets, scripts, less2css, html,
+   'clean', assets, scripts, less2css, html,
    gulp.parallel(stalker, server)
 ));
