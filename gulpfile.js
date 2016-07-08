f5572de15016891c7beaf9da2127519da0133796
Ulrik Moe <ulrik.moe@gmail.com>
2016-07-08 20:29:47 +0200

    Remove minification from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,40 +13,18 @@ const gulp = require('gulp');
 const del = require('del');
 const gutil = require('gulp-util');
 const connect = require('gulp-connect');
-const uglify = require('gulp-uglify');
 const nunjucks = require('gulp-nunjucks-html');
-const htmlmin = require('gulp-htmlmin');
 const less = require('gulp-less');
-const nano = require('gulp-cssnano');
-const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 
 //  Last changed
 const months = ['januar', 'februar', 'marts', 'april', 'maj', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'december'];
 const date = new Date();
-const updated = date.getDate() + '. ' + months[date.getMonth()] + ' ' + date.getFullYear();
 
-// Global variables
-const config = {
-    htmlmin: { removeComments: true, collapseWhitespace: true },
-    uglify: {
-        mangle: { sort: true, toplevel: true },
-        compress: {
-            drop_console: true,
-            loops: false,
-            unused: false
-        }
-    },
-    babel: {
-        plugins: ['transform-es2015-block-scoping', 'transform-es2015-shorthand-properties', 'transform-es2015-for-of']
-    },
-    nunjucks: {
-        searchPaths: ['www/'],
-        locals: { lastUpdate: updated }
-    }
-};
+let env = gutil.env;
+env.lastUpdate = date.getDate() + '. ' + months[date.getMonth()] + ' ' + date.getFullYear();
 
-function server() { connect.server({ root: 'www', livereload: !gutil.env.min }); }
+function server() { connect.server({ root: 'www', livereload: true }); }
 
 function assets() {
     return gulp.src(['src/assets/**/*', 'src/*.{ico,xml}'], { since: gulp.lastRun(assets) })
@@ -56,24 +34,20 @@ function assets() {
 
 function scripts() {
     return gulp.src(['src/js/data.js', 'src/js/kortgebyr.js'])
-    .pipe(gutil.env.min ? babel(config.babel) : gutil.noop())
     .pipe(concat('all.js'))
-    .pipe(gutil.env.min ? uglify(config.uglify) : gutil.noop())
-    .pipe(gulp.dest('www'));
+    .pipe(gulp.dest('www/js/'));
 }
 
 function less2css() {
     return gulp.src(['src/css/**/*'], { since: gulp.lastRun(less2css) })
     .pipe(less())
-    .pipe(gutil.env.min ? nano({ safe: true }) : gutil.noop())
-    .pipe(gulp.dest('www'))
+    .pipe(gulp.dest('www/css/'))
     .pipe(connect.reload());
 }
 
 function html() {
     return gulp.src(['src/**/*.html'])
-    .pipe(nunjucks(config.nunjucks))
-    .pipe(gutil.env.min ? htmlmin(config.htmlmin) : gutil.noop())
+    .pipe(nunjucks({ searchPaths: ['src/'], locals: env }))
     .pipe(gulp.dest('www'))
     .pipe(connect.reload());
 }
@@ -89,4 +63,4 @@ function stalker() {
 gulp.task('clean', function () { return del(['www/**', '!www']); });
 gulp.task('serve', gulp.parallel(stalker, server));
 gulp.task('build', gulp.series(assets, scripts, less2css, html));
-gulp.task('default', gulp.series('clean', assets, scripts, less2css, html, 'serve'));
+gulp.task('default', gulp.series('clean', 'build', 'serve'));
