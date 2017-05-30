861e5a4689ad3639dbc6b98f7d4d982260afec79
Ulrik Moe <ulrik.moe@gmail.com>
2017-05-30 13:28:51 +0200

    Revamp code (pull from dev branches)

--- src/js/currency.js
+++ src/js/currency.js
@@ -27,74 +27,30 @@ function Currency(value, code) {
 }
 
 Currency.prototype.print = function () {
-    const number = Math.round((this.dkk() * 100) / currency_value[$currency]) / 100;
-    const parts = number.toString().split('.');
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    const str = '' + Math.round(this.dkk() * 100 / currency_value[$currency]) / 100;
+    const sepIndex = str.indexOf('.');
+    const ints = (sepIndex === -1) ? str.length : sepIndex;
+    const curr = ' ' + currency_map[$currency];
+    let ret = '';
 
-    if (parts.length === 2 && parts[1].length === 1) {
-        parts[1] += '0';
+    for (let i = 0; i < ints; i++) {
+        ret += (i > 0 && (ints - i) % 3 === 0) ? '.' + str[i] : str[i];
     }
-    return parts.join(',') + ' ' + currency_map[$currency];
-};
-
-Currency.prototype.represent = function () {
-    if (this.length() === 1) {
-        for (let code in this.amounts) {
-            return this.amounts[code] + ' ' + code;
-        }
-    }
-    return this.dkk() / currency_value[$currency] + ' ' + $currency; //currency_map[$currency];
-};
-
-Currency.prototype.string = function () {
-    if (this.length() === 1) {
-        for (let code in this.amounts) {
-            return this.amounts[code] + code;
-        }
-    }
-    return this.dkk() / currency_value[$currency] + $currency; //currency_map[$currency];
+    return (sepIndex === -1) ? ret + curr : ret + ',' + str.substring(sepIndex + 1) + curr;
 };
 
 Currency.prototype.dkk = function () {
     let sum = 0;
     for (let code in this.amounts) {
-        if (this.amounts.hasOwnProperty(code) &&
-        currency_value.hasOwnProperty(code)) {
-            sum += currency_value[code] * this.amounts[code];
-        }
+        sum += currency_value[code] * this.amounts[code];
     }
     return sum;
 };
 
-/*
-Currency.prototype.add = function (o) {
-    if (this.code !== o.code) { throw 'differentCurrencyCodes'; }
-
-    const ret = this.zero();
-    let carry = 0;
-    let i = currencyDigits;
-    while (i--) {
-        const sum = this.value[i] + o.value[i] + carry;
-        if (sum > 9) {
-            carry = 1;
-            ret.value[i] = sum - 10;
-        }
-        else {
-            carry = 0;
-            ret.value[i] = sum;
-        }
-    }
-    if (carry > 0) { throw 'currencySumOverflow'; }
-    return ret;
-};
-*/
-
 Currency.prototype.add = function (o) {
     const n = new Currency(0, 'DKK');
     for (let code in this.amounts) {
-        if (this.amounts.hasOwnProperty(code)) {
-            n.amounts[code] = this.amounts[code];
-        }
+        n.amounts[code] = this.amounts[code];
     }
 
     for (let code in o.amounts) {
@@ -108,92 +64,10 @@ Currency.prototype.add = function (o) {
     return n;
 };
 
-
-Currency.prototype.is_equal_to = function (other_currency_object) {
-    for (let code in this.amounts) {
-        if (this.amounts.hasOwnProperty(code)) {
-            if (this.amounts[code] !== other_currency_object.amounts[code]) {
-                return false;
-            }
-        }
-    }
-    return true;
-};
-
 Currency.prototype.scale = function (rhs) {
     const n = new Currency(0, 'DKK');
     for (let code in this.amounts) {
-        if (this.amounts.hasOwnProperty(code)) {
-            n.amounts[code] = this.amounts[code] * rhs;
-        }
+        n.amounts[code] = this.amounts[code] * rhs;
     }
     return n;
 };
-
-
-
-
-function set_ccode(c) {
-    if (currency_map.hasOwnProperty(c)) {
-        $currency = c;
-    }
-}
-
-function mkcurregex() {
-    const arr = [];
-    for (let k in currency_map) {
-        arr.push(currency_map[k]);
-    }
-    for (let k in currency_value) {
-        arr.push(k);
-    }
-    return RegExp('^ *([0-9][0-9., ]*)(' + arr.join('|') + ')? *$', 'i');
-}
-const curregex = mkcurregex();
-
-function _getCurrency(currency) {
-    const a = currency.match(curregex);
-    if (a === null) { return null; }
-
-    let c = a[2] ? a[2] : currency_map[$currency];
-    if (c.toLowerCase() === currency_map[$currency].toLowerCase()) {
-        /* there are a lot of currencies named kr and we should prefer the kr
-        * that has been selected */
-        c = $currency;
-    } else {
-        /* if that's not what's selected, find the currency */
-        for (let k in currency_map) {
-            if (currency_map[k].toLowerCase() === c.toLowerCase() ||
-            k.toLowerCase() === c.toLowerCase()) {
-                c = k;
-                break;
-            }
-        }
-    }
-    return new Currency(parseFloat(a[1].replace('.', '').replace(',', '.')), c);
-}
-
-function getCurrency(currency, action) {
-
-    if (action === 'init') { $(currency).addEventListener('input', build); }
-
-    const a = _getCurrency($(currency).value);
-    if (a === null) {
-        $(currency).classList.add('error');
-        return null;
-    }
-    $(currency).classList.remove('error');
-    return a;
-}
-
-function changeCurrency() {
-    $('currency_code').innerHTML = this.value;
-    set_ccode(this.value);
-    build();
-    //save_url();
-}
-
-function setCurrency(k, v) {
-    $(k).value = v.represent();
-    $(k).classList.remove('error');
-}
--- src/js/main.js
+++ src/js/main.js
@@ -6,171 +6,52 @@
 function $(s) { return document.getElementById(s); }
 
 let $currency = 'DKK';
-let $avgvalue = new Currency(500, 'DKK');
 let $qty = 200;
-let $revenue = new Currency(500 * 200, 'DKK');
-//let $acquirer = ACQs;
-//let $cards = ['visa'];
-//let $features = [];
-//let $dankortscale = 0.77; // 77% of all trans to Dankort
+let $avgvalue = new Currency(500, $currency);
+let $revenue = $avgvalue.scale($qty);
+let $acqs = ACQs.slice(0); // Create copy
+let $cards = { dankort: 1, visa: 1 };
+let $features = {};
+let $dankortscale = 0.77; // 77% to Dankort. 23% to Visa/MC etc.
 
-function getInt(elem, action) {
-    if (action === 'init') {
-        elem.addEventListener('input', build, false);
+function updateSettings() {
+    const elems = this.elements;
+    $currency = elems.currency.value;
+    $avgvalue = new Currency(elems.avgvalue.value | 0, $currency);
+    $qty = elems.qty.value | 0;
+    $revenue = $avgvalue.scale($qty);
+    $cards = {};
+    $features = {};
+    $acqs = elems.acquirer.value; // Index
+
+    if ($acqs === 'auto') {
+        $acqs = ACQs.slice(0); // Copy of ACQs
+    } else {
+        $acqs = [ACQs[0], ACQs[$acqs]];
     }
 
-    const str = elem.value.trim();
-    if (!isNaN(parseFloat(str)) && isFinite(str) &&
-    parseFloat(str) === parseInt(str, 10)) {
-        elem.classList.remove('error');
-        return parseInt(str, 10);
-    }
-    elem.classList.add('error');
-    return null;
-}
-
-function setInt(k, v) {
-    $(k).value = parseInt(v, 10);
-    $(k).classList.remove('error');
-}
-
-function getPercent(k) {
-    const elem = $(k);
-    const str = elem.value.replace('%', '').replace(',', '.').trim();
-    if (!isNaN(parseFloat(str)) && isFinite(str)) {
-        $(k).classList.remove('error');
-        return parseFloat(str);
-    }
-    $(k).classList.add('error');
-    return null;
-}
-
-function setPercent(k, v) {
-    $(k).value = (parseFloat(v) + '%').replace('.', ',');
-    $(k).classList.remove('error');
-}
-
-
-const opts = {
-    cards: {
-        type: 'bits',
-        bits() { return document.getElementsByClassName('ocards').length; },
-        get(action) {
-            // Get all selected payment methods from .ocards
-            const obj = {};
-            const ocards = document.getElementsByClassName('ocards');
-            let bitval = 0;
-            for (let i = 0; i < ocards.length; i++) {
-                const checkbox = ocards[i];
-                if (checkbox.checked) {
-                    obj[checkbox.id] = 1;
-                    if (checkbox.id === 'visa') { obj.mastercard = 1; }
-                    bitval += 1 << i;
-                }
-            }
-            return obj;
-        },
-        set(bitval) {
-            const ocards = document.getElementsByClassName('ocards');
-            for (let i = 0; i < ocards.length; i++) {
-                const checkbox = ocards[i];
-                checkbox.checked = (bitval & (1 << i)) !== 0;
-            }
-        }
-    },
-    features: {
-        type: 'bits',
-        bits() { return document.getElementsByClassName('ofeatures').length; },
-        get(action) {
-            // Get all selected features
-            const obj = {};
-            const ofeatures = document.getElementsByClassName('ofeatures');
-            let bitval = 0;
-            for (let i = 0; i < ofeatures.length; i++) {
-                const checkbox = ofeatures[i];
-                if (checkbox.checked) {
-                    obj[checkbox.id] = 1;
-                    bitval += 1 << i;
-                }
-            }
-            //if (action === 'url') { return bitval; }
-            return obj;
-        },
-        set(bitval) {
-            const ofeatures = document.getElementsByClassName('ofeatures');
-            for (let i = 0; i < ofeatures.length; i++) {
-                const checkbox = ofeatures[i];
-                checkbox.checked = (bitval & (1 << i)) !== 0;
-            }
-        }
-    },
-    // Misc
-    acquirers: {
-        type: 'bits',
-        bits() {
-            let len = $('acquirer').length;
-            let nbits = 0;
-            while (len) {
-                len = len >>> 1;
-                nbits++;
-            }
-            return nbits;
-        },
-        get(action) {
-            // Return the selected acquirers
-            const index = $('acquirer').selectedIndex;
-            if (index) {
-                return [ACQs[0], ACQs[index]];
-            }
-            return ACQs.slice(0);
-        },
-        set(bitval) {
-            if (bitval < $('acquirer').length) { $('acquirer').selectedIndex = bitval; }
-        },
-    },
-    transactions: {
-        type: 'string',
-        dirty_bits: 1,
-        get_dirty_bits() { return +(this.get() !== parseInt($('transactions').defaultValue)); },
-        get(action) {
-            $qty = getInt($('transactions'), action);
-            return $qty;
-        },
-        set(v) {
-            setInt('transactions', v);
-        }
-    },
-    avgvalue: {
-        type: 'currency',
-        dirty_bits: 1,
-        get_dirty_bits() { return +(!this.get().is_equal_to(_getCurrency($('avgvalue').defaultValue))); },
-        get(action) {
-            $avgvalue = getCurrency('avgvalue', action);
-            return $avgvalue;
-        },
-        set(v) { setCurrency('avgvalue', _getCurrency(v)); }
-    },
-    currency: {
-        type: 'string',
-        dirty_bits: 1,
-        get_dirty_bits() { return +(this.get() !== $('currency_code_select').options[0].value); },
-        get() {
-            $revenue = $avgvalue.scale($qty);
-            return $currency;
-        },
-        set(v) {
-            const select = $('currency_code_select');
-            for (let i = 0; i < select.length; i++) {
-                if (select.options[i].value === v) {
-                    select.selectedIndex = i;
-                    $('currency_code').innerHTML = v;
-                    break;
-                }
-            }
-            set_ccode(v);
+    const cards = elems['cards[]'];
+    for (let i = 0; i < cards.length; i++) {
+        if (cards[i].checked) {
+            $cards[cards[i].value] = 1;
         }
     }
-};
+
+    const features = elems['features[]'];
+    for (let i = 0; i < features.length; i++) {
+        if (features[i].checked) {
+            $features[features[i].value] = 1;
+        }
+    }
+
+    $dankortscale = (!$cards.visa) ? 1 : ($cards.dankort || $cards.forbrugsforeningen) ? 0.77 : 0;
+
+    $('tbody').innerHTML = '';
+    if ($cards.dankort || $cards.visa) {
+        build();
+    }
+    $('currency_code').textContent = $currency;
+}
 
 
 // Check if object-x' properties is in object-y.
@@ -180,17 +61,6 @@ function x_has_y(objx, objy) {
     }
     return true;
 }
-
-// To do: Settings skal forenes med opts()
-const Settings = function (action) {
-    //if (action === 'init') { loadurl(); }
-
-    for (let key in opts) {
-        if (key !== 'dirty_bits') { this[key] = opts[key].get(action); }
-    }
-    this.features['3-D secure'] = true;
-};
-
 
 function sum() {
     let sumobj = new Currency(0, 'DKK');
@@ -204,15 +74,15 @@ function sum() {
 }
 
 // Find combination of acquirers that support all cards
-function acqcombo(psp, settings) {
-    const A = settings.acquirers;
+function acqcombo(psp) {
+    const A = $acqs;
     const acqarr = [];
 
     // Check if a single acq support all cards.
     for (let acq of A) {
         if (psp.acquirers[acq.name]) {
-            // Return acq if it support all settings.cards.
-            if (x_has_y(acq.cards, settings.cards)) { return [acq]; }
+            // Return acq if it support all $cards.
+            if (x_has_y(acq.cards, $cards)) { return [acq]; }
             acqarr.push(acq);
         }
     }
@@ -223,7 +93,7 @@ function acqcombo(psp, settings) {
         const primary = acqarr[i];
         let missingCards = {};
 
-        for (let card in settings.cards) {
+        for (let card in $cards) {
             if (!primary.cards[card]) { missingCards[card] = true; }
         }
 
@@ -239,26 +109,34 @@ function acqcombo(psp, settings) {
 }
 
 
+function sumTxt(obj) {
+    const frag = document.createDocumentFragment();
+    frag.textContent = sum(obj).print();
+    if (Object.keys(obj).length) {
+        const info = document.createElement('div');
+        info.textContent = '[?]';
+        info.className = 'info';
+        info.ttdata = obj;
+        info.addEventListener('mouseover', showTooltip);
+        frag.appendChild(info);
+    }
+    return frag;
+}
+
+
 // Build table
 function build(action) {
-    settings = new Settings(action);
     const data = [];
-    const tbody = document.createElement('tbody');
-    tbody.id = 'tbody';
-
-    // Input validation
-    if (!Object.keys(settings.cards).length) { return false; }
-
-    // Cards
-    const dankortscale = (!settings.cards.visa) ? 1 : (settings.cards.dankort) ? 0.77 : 0;
+    const frag = document.createDocumentFragment();
 
     // Calculate acquirer costs and sort by Total Costs.
-    for (let acq of settings.acquirers) {
-        const cardscale = (acq.name === 'Nets') ? dankortscale : 1 - dankortscale;
-        acq.trnfees = acq.fees.trn(settings).scale(settings.transactions).scale(cardscale);
-        acq.TC = acq.trnfees.add(acq.fees.monthly);
+    for (let acq of $acqs) {
+        const cardscale = (acq.name === 'Nets') ? $dankortscale : 1 - $dankortscale;
+        acq.trnfees = acq.fees.trn().scale($qty).scale(cardscale);
+        acq.TC = acq.trnfees;
+        if (acq.fees.monthly) { acq.TC = acq.TC.add(acq.fees.monthly); }
     }
-    settings.acquirers.sort(function (obj1, obj2) { return obj1.TC.dkk() - obj2.TC.dkk(); });
+    $acqs.sort(function (obj1, obj2) { return obj1.TC.dkk() - obj2.TC.dkk(); });
 
     psploop:
     for (let psp of PSPs) {
@@ -266,37 +144,37 @@ function build(action) {
         const monthly = {};
         const trnfee = {};
 
-        setup[psp.name] = psp.fees.setup;
-        monthly[psp.name] = psp.fees.monthly;
-        trnfee[psp.name] = psp.fees.trn(settings);
+        if (psp.fees.setup) { setup[psp.name] = psp.fees.setup; }
+        if (psp.fees.monthly) { monthly[psp.name] = psp.fees.monthly; }
+        if (psp.fees.trn && psp.fees.trn()) { trnfee[psp.name] = psp.fees.trn(); }
 
         // Check if psp support all enabled payment methods
-        for (let card in settings.cards) { if (!psp.cards[card]) { continue psploop; } }
+        for (let card in $cards) {
+            if (!psp.cards[card]) { continue psploop; }
+        }
 
         // Check if psp support all enabled features
-        for (let i in settings.features) {
-            if (!psp.features || !psp.features[i]) { continue psploop; }
+        for (let i in $features) {
             const feature = psp.features[i];
-            if (feature.setup) {
-                setup[i] = feature.setup;
-                monthly[i] = feature.monthly;
-                trnfee[i] = feature.trn;
-            }
+            if (!feature) { continue psploop; }
+            if (feature.setup) { setup[i] = feature.setup; }
+            if (feature.monthly) { monthly[i] = feature.monthly; }
+            if (feature.trn) { trnfee[i] = feature.trn; }
         }
 
         // If an acquirer has been selected then hide the Stripes
-        if (settings.acquirers.length < 3 && !psp.acquirers) { continue; }
+        if ($acqs.length < 3 && !psp.acquirers) { continue; }
 
         const acqfrag = document.createDocumentFragment();
         const acqcards = {};
         let acqArr = [];
         if (psp.acquirers) {
+            acqArr = acqcombo(psp); // Find acq with full card support
 
-            acqArr = acqcombo(psp, settings); // Find acq with full card support
             if (!acqArr) { continue; }
             for (let acq of acqArr) {
-                setup[acq.name] = acq.fees.setup;
-                monthly[acq.name] = acq.fees.monthly;
+                if (acq.fees.setup) { setup[acq.name] = acq.fees.setup; }
+                if (acq.fees.monthly) { monthly[acq.name] = acq.fees.monthly; }
                 trnfee[acq.name] = acq.trnfees;
 
                 const acqlink = document.createElement('a');
@@ -321,15 +199,14 @@ function build(action) {
 
         const cardfrag = document.createDocumentFragment();
         for (let card in psp.cards) {
-
             if (psp.acquirers && !acqcards[card]) { continue; }
 
             //  Some cards/methods (e.g. mobilepay) add extra costs.
-            if (psp.cards[card].setup) {
-                if (!settings.cards[card]) { continue; } // Disable if not enabled.
-                setup[card] = psp.cards[card].setup;
-                monthly[card] = psp.cards[card].monthly;
-                trnfee[card] = psp.cards[card].trn;
+            if (typeof psp.cards[card] === 'object') {
+                if (!$cards[card]) { continue; }
+                if (psp.cards[card].setup) { setup[card] = psp.cards[card].setup; }
+                if (psp.cards[card].monthly) { monthly[card] = psp.cards[card].monthly; }
+                if (psp.cards[card].trn) { trnfee[card] = psp.cards[card].trn; }
             }
 
             const cardicon = new Image(22, 15);
@@ -353,7 +230,7 @@ function build(action) {
         psplink.target = '_blank';
         psplink.href = psp.link;
         psplink.className = 'psp';
-        const psplogo = new Image(psp.w, psp.h);
+        const psplogo = new Image();
         psplogo.src = '/img/psp/' + psp.logo + '?{{ imgtoken }}';
         psplogo.alt = psp.name;
         const pspname = document.createElement('span');
@@ -362,56 +239,27 @@ function build(action) {
         psplink.appendChild(pspname);
         pspfrag.appendChild(psplink);
 
-        // setup fees
-        const setupfrag = document.createDocumentFragment();
-        setupfrag.textContent = sum(setup).print();
-        const setup_info = document.createElement('div');
-        setup_info.textContent = '[?]';
-        setup_info.className = 'info';
-        setup_info.ttdata = setup;
-        setup_info.addEventListener('mouseover', showTooltip);
-        setupfrag.appendChild(setup_info);
-
-        // Recurring fees
-        const recurringfrag = document.createDocumentFragment();
-        recurringfrag.textContent = sum(monthly).print();
-        const recurring_info = document.createElement('div');
-        recurring_info.textContent = '[?]';
-        recurring_info.className = 'info';
-        recurring_info.ttdata = monthly;
-        recurring_info.addEventListener('mouseover', showTooltip);
-        recurringfrag.appendChild(recurring_info);
-
-        // Trn fees
-        const trnfrag = document.createDocumentFragment();
-        trnfrag.textContent = sum(trnfee).print();
-        const trn_info = document.createElement('div');
-        trn_info.textContent = '[?]';
-        trn_info.className = 'info';
-        trn_info.ttdata = trnfee;
-        trn_info.addEventListener('mouseover', showTooltip);
-        trnfrag.appendChild(trn_info);
-
         // cardfee calc.
         const cardfeefrag = document.createDocumentFragment();
         const p1 = document.createElement('p');
-        const cardfee = totalcost.scale(1 / (settings.transactions || 1));
+        const cardfee = totalcost.scale(1 / ($qty || 1));
         cardfeefrag.textContent = cardfee.print();
-        p1.textContent = '(' + (cardfee.scale(1 / settings.avgvalue.dkk()).dkk() * 100).toFixed(3).replace('.', ',') + '%)';
+        p1.textContent = '(' + (cardfee.scale(1 / $avgvalue.dkk()).dkk() * 100).toFixed(3).replace('.', ',') + '%)';
         p1.className = 'procent';
         cardfeefrag.appendChild(p1);
 
-        const row = tbody.insertRow(sort);
-        row.insertCell(-1).appendChild(pspfrag);
-        row.insertCell(-1).appendChild(acqfrag);
-        row.insertCell(-1).appendChild(cardfrag);
-        row.insertCell(-1).appendChild(setupfrag);
-        row.insertCell(-1).appendChild(recurringfrag);
-        row.insertCell(-1).appendChild(trnfrag);
-        row.insertCell(-1).textContent = totalcost.print();
-        row.insertCell(-1).appendChild(cardfeefrag);
+        const tr = document.createElement('tr');
+        tr.insertCell(-1).appendChild(pspfrag);
+        tr.insertCell(-1).appendChild(acqfrag);
+        tr.insertCell(-1).appendChild(cardfrag);
+        tr.insertCell(-1).appendChild(sumTxt(setup));
+        tr.insertCell(-1).appendChild(sumTxt(monthly));
+        tr.insertCell(-1).appendChild(sumTxt(trnfee));
+        tr.insertCell(-1).textContent = totalcost.print();
+        tr.insertCell(-1).appendChild(cardfeefrag);
+        frag.insertBefore(tr, frag.childNodes[sort]);
     }
-    $('table').replaceChild(tbody, $('tbody'));
+    $('tbody').appendChild(frag);
 }
 
 //===========================
@@ -419,5 +267,6 @@ function build(action) {
 //===========================
 
 build('init');
-$('currency_code_select').addEventListener('change', changeCurrency);
-$('form').addEventListener('change', build);
+// TODO: Fix 2x build on some input changes.
+$('form').addEventListener('change', updateSettings);
+$('form').addEventListener('input', updateSettings);
